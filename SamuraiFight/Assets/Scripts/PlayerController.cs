2c59cad69413e24bf28d48a4b71285b3bcfe673b
Axel <anatoly.garchik@gmail.com>
2020-10-29 00:02:33 +0300

    added move player

--- SamuraiFight/Assets/Scripts/PlayerController.cs
+++ SamuraiFight/Assets/Scripts/PlayerController.cs
@@ -4,27 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Animator _animator;
+    private Rigidbody2D _rigidbody;
     private float _speed = 6f;
-    private float _jumpHeight = 10f;
-    private Animator animator;
-    private Rigidbody2D rigidbody;
+    private float _jumpHeight = 7f;
+    private bool _isGrounded = true;
 
-    [SerializeField] private GameObject GroundCheck;
-    [SerializeField] private Joystick Joystick;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private Joystick _joystick;
+    [SerializeField] private LayerMask _whatIsGround;
 
-    void Start()
+    private void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
-    void Update()
+    private void Update()
     {
-        rigidbody.velocity = new Vector2(Joystick.Horizontal * _speed, rigidbody.velocity.y);
-        if (Joystick.Vertical >= .5f)
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(_groundCheck.position, 0.2f, _whatIsGround);
+        _isGrounded = colliders.Length > 1;
+        if (_joystick.Vertical >= .5f && _isGrounded)
         {
-            /*rigidbody.AddForce(transform.up * _jumpHeight, ForceMode2D.Impulse);
-            rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, _jumpHeight);*/
-            rigidbody.velocity = Vector2.up * _jumpHeight;
+            _rigidbody.velocity = Vector2.up * _jumpHeight;
         }
+        if (_joystick.Horizontal > 0)
+        {
+
+        }
+        _rigidbody.velocity = new Vector2(_joystick.Horizontal * _speed, _rigidbody.velocity.y);
     }
+
+
 }
