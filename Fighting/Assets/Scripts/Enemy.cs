9c18e80861c7ea88c925b247bdce8f9bb90d72f2
Axel <anatoly.garchik@gmail.com>
2020-10-31 01:11:04 +0300

    added enemy Samurai

---
+++ Fighting/Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : PlayerController
+{
+    private void Start()
+    {
+        _health = 100f;
+        _damage = 10f;
+    }
+
+    private void Update()
+    {
+        print(_health);
+        TakeDamage(7f);
+
+    }
+}
--- Fighting/Assets/Scripts/PlayerController.cs
+++ Fighting/Assets/Scripts/PlayerController.cs
@@ -16,63 +16,59 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private LayerMask _whatIsGround;
     [SerializeField] private GameObject _sword;
 
+    protected float _health;
+    protected float _armor;
+    protected float _damage;
+
+
+    public void OnAttackButtonDown()
+    {
+        if (!_isAttacking)
+        {
+            _isAttacking = true;
+            _animator.SetInteger("State", 7);
+            StartCoroutine(DoAttack());
+        }
+
+    }
+
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
         _sword.SetActive(false);
+        _health = 100f;
+        _damage = 7f;
     }
     private void Update()
     {
         GroundCheck();
-        JumpPlayer();
         Flip();
     }
 
     private void FixedUpdate()
     {
-        if (_joystick.Horizontal != 0)
-        {
-            _rigidbody.velocity = new Vector2(_joystick.Horizontal * _speed, _rigidbody.velocity.y);
-            _animator.SetInteger("State", 2);
-            _isGrounded = true;
-        }
+        MovePlayer();
     }
 
     private void MovePlayer()
     {
-        if (_joystick.Horizontal != 0)
+        if (_joystick.Horizontal != 0 && !_isAttacking)
         {
             _rigidbody.velocity = new Vector2(_joystick.Horizontal * _speed, _rigidbody.velocity.y);
             _animator.SetInteger("State", 2);
         }
-        else if (!_isAttacking)
+        else if (_joystick.Horizontal == 0 && !_isAttacking)
         {
             _animator.SetInteger("State", 1);
         }
-    }
 
-    private void JumpPlayer()
-    {
         if (_joystick.Vertical >= .9f && _isGrounded && !_isAttacking)
         {
-            //_rigidbody.velocity = Vector2.up * _jumpHeight;
-            _rigidbody.AddForce(transform.up * _jumpHeight, ForceMode2D.Impulse);
-            _rigidbody.velocity = Vector3.ClampMagnitude(_rigidbody.velocity, _jumpHeight);
+            _rigidbody.velocity = Vector2.up * _jumpHeight;
+            //_rigidbody.AddForce(transform.up * _jumpHeight, ForceMode2D.Impulse);
+            // _rigidbody.velocity = Vector3.ClampMagnitude(_rigidbody.velocity, _jumpHeight);
             _animator.SetInteger("State", 3);
-        }
-    }
-
-    public void OnJumpButtonDown()
-    {
-        print("NotJump");
-        
-        if (_isGrounded)
-        {
-            _rigidbody.AddForce(transform.up * _jumpHeight, ForceMode2D.Impulse);
-            _rigidbody.velocity = Vector3.ClampMagnitude(_rigidbody.velocity, _jumpHeight);
-            //_rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpHeight);
-            print("jump");
         }
     }
 
@@ -92,32 +88,32 @@ public class PlayerController : MonoBehaviour
     {
         Collider2D[] colliders = Physics2D.OverlapCircleAll(_groundCheck.position, 0.2f, _whatIsGround);
         _isGrounded = colliders.Length > 1;
-        if (_isGrounded)
-        {
-            print("groundOn");
-        }
-        if (!_isGrounded)
-        {
-            print("groundOff");
-        }
-    }
-
-    public void OnAttackButtonDown()
-    {
-        if (!_isAttacking)
-        {
-            _isAttacking = true;
-            //_animator.SetInteger("State", 7);
-            _animator.Play("Swordsman_attack");
-            StartCoroutine(DoAttack());
-        }
     }
 
     private IEnumerator DoAttack()
     {
         _sword.SetActive(true);
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(.4f);
         _sword.SetActive(false);
         _isAttacking = false;
+    }
+
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Sword")
+        {
+            _health -= _damage;
+            print(_health);
+
+            if (_health < 0)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    protected void TakeDamage(float damage)
+    {
+
     }
 }
